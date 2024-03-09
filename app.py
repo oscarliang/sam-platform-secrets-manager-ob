33a590c42916a2a0737d529250d298661e97f0ac
Heshan Harsha <heshan039@gmail.com>
2024-03-09 12:32:22 +0530

    updates

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pprint
 # Load the YAML file
 with open('secrets.yaml', 'r') as file:
     secrets_config = yaml.safe_load(file)
-# print(secrets_config);
+print(secrets_config);
 
 
 # Extract the KMS key ID for the specific secret
@@ -13,25 +13,46 @@ kms_key_id = None
 for secret in secrets_config['secrets']:
     if secret['name'] == 'nonprod-sql-rds-dbsecret':
         kms_key_id = secret['kms key id']
-        print(kms_key_id);
+        #print(kms_key_id);
         break
 
 if not kms_key_id:
     raise ValueError("KMS key ID not found in secrets.yaml")
 
-# Assume 'nonprod-sql-rds-dbsecret.enc' is the encrypted secret file
-# You would need to replace this part with the actual decryption logic
-# with open('nonprod-sql-rds-dbsecret.enc', 'r') as enc_file:
-#     secret_value = decrypt_function(enc_file.read())  # Replace with actual decryption
+# Create a KMS client
+kms_client = boto3.client('kms')
+
+# Read the encrypted data
+with open('nonprod-sql-rds-dbsecret.enc', 'rb') as enc_file:
+    encrypted_data = enc_file.read()
+    #print(encrypted_data);
+
+# Decrypt the data
+decrypt_response = kms_client.decrypt(CiphertextBlob=encrypted_data, KeyId=kms_key_id)
+decrypted_data = decrypt_response['Plaintext']
+# print(decrypted_data);
+
+decrypted_secret = decrypted_data.decode('utf-8') 
+# print(decrypted_secret);
 
 # Create a Secrets Manager client
-# client = boto3.client('secretsmanager')
+secretsmanager_client = boto3.client('secretsmanager')
 
-# Create the secret in AWS Secrets Manager
-# response = client.create_secret(
-#     Name=secrets_config['name'],
-#     Description=secrets_config['description'],
-#     SecretString=secret_value
-# )
+# Iterate over each secret in the list of secrets
+for secret in secrets_config['secrets']:
+    # Access the 'name' of the current secret
+    secret_name = secret['name']
+    print(secret_name)
 
-# print(response)
+    # Access the 'description' of the current secret
+    secret_description = secret['description']
+    print(secret_description)
+
+    response = secretsmanager_client.create_secret(
+        Name=secret_name,
+        Description=secret_description,
+        SecretString=decrypted_secret
+    )
+    
+    # Print the response from AWS Secrets Manager
+    print(response)
