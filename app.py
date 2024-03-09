9b636f7d77aaaa60c4de7b06d328cf44a4fab7fd
Heshan Harsha <heshan039@gmail.com>
2024-03-09 12:47:12 +0530

    updates

--- app.py
+++ app.py
@@ -5,35 +5,9 @@ from botocore.exceptions import ClientError
 # Load the YAML file
 with open('secrets.yaml', 'r') as file:
     secrets_config = yaml.safe_load(file)
-print(secrets_config);
-
-
-# Extract the KMS key ID for the specific secret
-kms_key_id = None
-for secret in secrets_config['secrets']:
-    if secret['name'] == 'nonprod-sql-rds-dbsecret':
-        kms_key_id = secret['kms key id']
-        #print(kms_key_id);
-        break
-
-if not kms_key_id:
-    raise ValueError("KMS key ID not found in secrets.yaml")
 
 # Create a KMS client
 kms_client = boto3.client('kms')
-
-# Read the encrypted data
-with open('nonprod-sql-rds-dbsecret.enc', 'rb') as enc_file:
-    encrypted_data = enc_file.read()
-    #print(encrypted_data);
-
-# Decrypt the data
-decrypt_response = kms_client.decrypt(CiphertextBlob=encrypted_data, KeyId=kms_key_id)
-decrypted_data = decrypt_response['Plaintext']
-# print(decrypted_data);
-
-decrypted_secret = decrypted_data.decode('utf-8') 
-# print(decrypted_secret);
 
 # Create a Secrets Manager client
 secretsmanager_client = boto3.client('secretsmanager')
@@ -42,6 +16,17 @@ secretsmanager_client = boto3.client('secretsmanager')
 for secret in secrets_config['secrets']:
     secret_name = secret['name']
     secret_description = secret['description']
+    kms_key_id = secret['kms_key_id'] 
+
+    # Read the encrypted data
+    with open('nonprod-sql-rds-dbsecret.enc', 'rb') as enc_file:
+        encrypted_data = enc_file.read()
+
+    # Decrypt the data
+    decrypt_response = kms_client.decrypt(CiphertextBlob=encrypted_data, KeyId=kms_key_id)
+    decrypted_data = decrypt_response['Plaintext']
+
+    decrypted_secret = decrypted_data.decode('utf-8') 
 
     try:
         response = secretsmanager_client.create_secret(
