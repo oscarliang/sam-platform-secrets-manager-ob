ad11c8e961234d388ee2767639c74428a64849d3
Heshan Harsha <heshan039@gmail.com>
2024-04-09 20:39:57 +0530

    Updates

--- app.py
+++ app.py
@@ -30,55 +30,60 @@ secretsmanager_client = boto3.client('secretsmanager')
 
 # Iterate over each secret in the list of secrets
 for secret in secrets_config['secrets']:
-    secret_name = secret['name']
-    secret_description = secret['description']
-    kms_key_id = secret['kms_key_id']
-    encryted_file = secrets_folder + secret['name']+".enc"
-    role_arns = secret.get('role_arns', [])
+    if secret.get('isConsumableSecret', False):
+        secret_name = secret['name']
+        secret_description = secret['description']
+        role_arns = secret.get('role_arns', [])
+        policy = {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Principal": {"AWS": role_arns},
+                    "Action": "secretsmanager:GetSecretValue",
+                    "Resource": "*"
+                }
+            ]
+        }
 
-    # Construct the policy document
-    policy = {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Principal": {"AWS": role_arns},
-                "Action": "secretsmanager:GetSecretValue",
-                "Resource": "*"
-            }
-        ]
-    }
+        policy_str = json.dumps(policy)
 
-    # Convert the policy document to a JSON string
-    policy_str = json.dumps(policy)
-
-    # Read the encrypted data
-    with open(encryted_file, 'rb') as enc_file:
-        encrypted_data = enc_file.read()
-
-    # Decrypt the data
-    decrypt_response = kms_client.decrypt(CiphertextBlob=encrypted_data, KeyId=kms_key_id)
-    decrypted_data = decrypt_response['Plaintext']
-    decrypted_secret = decrypted_data.decode('utf-8') 
-
-    try:
-        response = secretsmanager_client.create_secret(
-            Name=secret_name,
-            Description=secret_description,
-            SecretString=decrypted_secret,
-        )
-        
         try:
-            response = secretsmanager_client.put_resource_policy(
+            secretsmanager_client.update_secret(
+                SecretId=secret_name,
+                Description=secret_description
+            )
+            
+            secretsmanager_client.put_resource_policy(
                 SecretId=secret_name,
                 ResourcePolicy=policy_str
             )
-            print(f"Policy attached to {secret_name}")
-        except Exception as e:
-            print(f"Error attaching policy to {secret_name}: {e}")
+            print(f"Updated secret {secret_name} with new description and policy.")
+        except ClientError as e:
+            print(f"Error updating secret {secret_name}: {e}")
+    else:
+        # Process secrets with isConsumableSecret: false
+        secret_name = secret['name']
+        kms_key_id = secret['kms_key_id']
+        encrypted_file = secrets_folder + secret['name'] + ".enc"
 
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'ResourceExistsException':
-            print(f"Secret {secret_name} already exists. Skipping creation.")
-        else:
-            raise
+        # Read and decrypt the data
+        with open(encrypted_file, 'rb') as enc_file:
+            encrypted_data = enc_file.read()
+
+        decrypt_response = kms_client.decrypt(CiphertextBlob=encrypted_data, KeyId=kms_key_id)
+        decrypted_data = decrypt_response['Plaintext']
+        decrypted_secret = decrypted_data.decode('utf-8')
+
+        try:
+            # Create the secret in Secrets Manager
+            secretsmanager_client.create_secret(
+                Name=secret_name,
+                SecretString=decrypted_secret
+            )
+            print(f"Created secret {secret_name}.")
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'ResourceExistsException':
+                print(f"Secret {secret_name} already exists. Skipping creation.")
+            else:
+                raise
